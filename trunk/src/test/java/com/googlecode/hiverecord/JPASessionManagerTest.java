2066a745c3fd32c7fb39d02f535b5467901b954a
minslovey <minslovey@50993408-957c-812a-3e1e-60e21493f0dc>
2010-03-18 16:01:44 +0000

    Added some unit-test methods on JPASessionManagerTest

---
+++ trunk/src/test/java/com/googlecode/hiverecord/JPASessionManagerTest.java
@@ -0,0 +1,67 @@
+package com.googlecode.hiverecord;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.googlecode.hiverecord.support.Message;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JPASessionManagerTest {
+	JPASessionManager<Message> O = new JPASessionManager<Message>(null);
+	@Mock
+	EntityManager manager;
+	@Mock
+	EntityManagerFactory factory;
+	@Mock
+	EntityTransaction transaction;
+	
+	@Before
+	public void prepereFactoryMock() {
+		when(factory.createEntityManager()).thenReturn(manager);
+		O.factory = factory;
+	}
+
+	@Before
+	public void prepereTransactionMock() {
+		when(manager.getTransaction()).thenReturn(transaction);
+	}
+	
+	@Test(expected=HiveRecordException.class)
+	public void transactionShouldBeRollbackedAndClosedWhenCanNotFind() throws Exception {
+		when(manager.find(Message.class, 1L)).thenThrow(new RuntimeException());
+		
+		try {
+			O.find(Message.class, 1L);
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			assertThat(O.rollbacked, is(true));
+			verify(manager).close();
+		}
+	}
+
+	@Test
+	public void transactionShouldBeCommitedAndClosedWhenCanFind() throws Exception {
+		Message expectedMesssage = new Message("expected");
+		when(manager.find(Message.class, 1L)).thenReturn(expectedMesssage);
+		
+		Message result = O.find(Message.class, 1L);
+		
+		assertThat(result, equalTo(expectedMesssage));
+		verify(transaction).commit();
+		verify(manager).close();
+	}
+}
